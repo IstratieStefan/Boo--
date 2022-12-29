ea19b86d305dacfeebc67aecde1de95c57ed2cec
IstratieStefan <istratie.stefan123@gmail.com>
2022-12-29 22:54:39 +0200

    BOO!

---
+++ Boo.h
@@ -0,0 +1,95 @@
+#include <iostream>
+
+//  C++ keywords
+#define BooSpace namespace
+#define BooUsing using
+#define TemplateForBooing template
+#define BooType typename
+#define Boooooo typedef
+
+#define BooClass class
+#define BooStruct struct
+#define Boonion union
+
+#define TureBoo true
+#define FalseBoo false
+#define NoBoo !
+#define _NoBoo not
+
+#define LongBoo long
+#define UnsignedBoo unsigned
+#define SignedBoo signed
+
+#define ZERO_BOO NULL
+#define _ZERO_BOO nullptr
+#define ReturnTheBoo return
+
+#define PrivateBoo private
+
+#define NewBoo new
+#define EraseBoo delete
+
+#define StopBoo break
+#define ContinueBoo continue
+
+#define BOOO this
+
+#define While_BOO while
+#define BOO_for for
+#define BOOOO do
+#define SwitchTheBoo switch
+#define CaseBoo case
+
+#define Boo? if
+#define Boo! else
+
+
+#define And_BOO &&
+#define _And_BOO and
+
+
+#define TryBoo try
+#define CaughtYou catch
+#define DefaultBoo default
+
+
+#define prieten friend
+#define protejat protected
+
+#define mei_la goto
+
+#define in_linie inline
+
+#define registru register
+
+#define reinterpreteaza reinterpret_cast
+
+
+
+
+
+//  Misc
+#define constant_boo const
+
+//  STD stuff
+#define Boo_in std::cin
+#define Boo_out std::cout
+#define Booo_next std::endl
+
+//  String library stuff
+#define Get_boo_line std::cin.getline
+
+//  #trolling
+#define Hey {
+#define Bye }
+
+#define bOO {
+#define OOb }
+
+#define BOO_Stop break
+#define BOO_Continue continue
+
+//  More trolling
+#define BOO() main()
+#define Booooooo return(0)
+
